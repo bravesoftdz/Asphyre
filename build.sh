75a0461373ad5f96918cfdf7c1a34a2b6bcc1a7d
markfirmware <markfirmware@users.noreply.github.com>
2017-05-18 09:27:54 -0400

    MountImage issue in Samples/FreePascal/Ultibo/Basic

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Build the ultibo projects in this repo.
-#
-# Runs on linux/x64 with either ultibo installed or docker installed.
-# Runs on windows git bash with ultibo installed.
-# Runs on windows 10 pro git bash with docker installed.
-
 function log {
     echo $* | tee -a $LOG
 }
@@ -15,7 +9,7 @@ function ultibo-bash-quotation {
     then
         echo -n $*
     else
-        local DOCKER_IMAGE=markfirmware/ultibo-bash
+        local DOCKER_IMAGE=markfirmware/ultibo-1.3.271-x64-1.0.0
         echo -en "docker run --rm -i -v $(pwd):/workdir --entrypoint /bin/bash $DOCKER_IMAGE -c \"$*\""
     fi
 }
@@ -24,18 +18,107 @@ function ultibo-bash {
     eval $(ultibo-bash-quotation $*)
 }
 
+function unix_line_endings {
+    tr -d \\r < $1 > tmp && \
+    mv tmp $1
+}
+
+function convert-frames {
+    ls frame*-1920x1080x3.fb > /dev/null 2>&1
+    if [[ $? -eq 0 ]]
+    then
+        for frame in frame*-1920x1080x3.fb
+        do
+            ultibo-bash convert -size 1920x1080 -depth 8 rgb:$frame ${frame%.fb}.png && \
+            rm $frame
+        done
+    fi
+}
+
+function test-qemu-controller {
+    echo .... running qemu
+    local RESTORE_PWD=$(pwd)
+    echo $RESTORE_PWD
+    cd $ARTIFACTS/QEMUVPB
+    pwd
+    time python $RESTORE_PWD/run-qemu
+    if [[ $? -ne 0 ]]; then log fail: $?; fi
+    cd test
+
+    for textfile in *.txt
+    do
+        unix_line_endings $textfile
+    done
+    sed -i 's/.\x1b.*\x1b\[D//' qemumonitor.txt
+    sed -i 's/\x1b\[K//' qemumonitor.txt
+    ls screen*.ppm > /dev/null 2>&1
+    if [[ $? -eq 0 ]]
+    then
+        for screen in screen*.ppm
+        do
+            ultibo-bash convert $screen ${screen%.ppm}.png && \
+            rm $screen
+        done
+    fi
+    convert-frames
+
+    file *
+
+#   grep -i error applog.txt	
+#   local EXIT_STATUS=$?
+
+    cd $RESTORE_PWD
+#   if [[ EXIT_STATUS == 0 ]]; then log fail: $?; fi
+}
+
+function build-ultibo-asphyre-basic {
+    local src=Samples/FreePascal/Ultibo/Basic
+    build-as QEMUVPB $src
+#   build-as RPI $src
+#   build-as RPI2 $src
+#   build-as RPI3 $src
+}
+
+function build-as {
+    local CONTROLLER=$1
+    local SRC_FOLDER=$2
+    local LPR_FILE=
+    if [[ -d $SRC_FOLDER ]]
+    then
+        ls $SRC_FOLDER/*.lpr > /dev/null 2>&1
+        if [[ $? -eq 0 ]]
+        then
+            LPR_FILE=$SRC_FOLDER/*.lpr
+        fi
+        if [[ $LPR_FILE != "" ]]
+        then
+            case $CONTROLLER in
+                QEMUVPB)
+                    build-lpr $SRC_FOLDER $LPR_FILE $CONTROLLER CONTROLLER_QEMUVPB "-CpARMV7A -WpQEMUVPB" qemuvpb.cfg ;;
+#                   test-qemu-controller ;;
+                RPI)
+                    build-lpr $SRC_FOLDER $LPR_FILE $CONTROLLER CONTROLLER_RPI_INCLUDING_RPI0 "-CpARMV6 -WpRPIB" rpi.cfg ;;
+                RPI2)
+                    build-lpr $SRC_FOLDER $LPR_FILE $CONTROLLER CONTROLLER_RPI2_INCLUDING_RPI3 "-CpARMV7A -WpRPI2B" rpi2.cfg ;;
+                RPI3)
+                    build-lpr $SRC_FOLDER $LPR_FILE $CONTROLLER CONTROLLER_RPI3 "-CpARMV7A -WpRPI3B" rpi3.cfg ;;
+            esac
+        fi
+    fi
+}
+
 function build-lpr {
-    local LPR_FILE=$1
-    local TARGET_COMPILER_OPTIONS=$2
-    local CFG_NAME=$3
-    local LPR_FOLDER=$(dirname $LPR_FILE)
-    local INCLUDES=-Fi/root/ultibo/core/fpc/source/packages/fv/src
-    log
-    log
-    log .... building $LPR_FILE
-    rm -rf $LPR_FOLDER/obj && \
-    mkdir -p $LPR_FOLDER/obj && \
+    local SRC_FOLDER=$1
+    local LPR_FILE=$2
+    local CONTROLLER=$3
+    local CONTROLLER_SYMBOL=$4
+    local CONTROLLER_COMPILER_OPTIONS=$5
+    local CFG_NAME=$6
+    local INCLUDES="-Fi/root/ultibo/core/fpc/source/packages/fv/src -Fi/root/ultibo/core/fpc/source/rtl/ultibo/core"
+    log .... building $LPR_FILE $CONTROLLER
+    mkdir -p $ARTIFACTS/$CONTROLLER $OBJ/$CONTROLLER && \
     ultibo-bash fpc \
+     -d$CONTROLLER_SYMBOL \
      -l- \
      -v0ewn \
      -B \
@@ -44,53 +127,15 @@ function build-lpr {
      -Parm \
      -Mdelphi \
      -FuSource \
-     -FE$LPR_FOLDER/obj \
+     -Fugh/ultibohub/Asphyre/Source \
+     -FE$OBJ/$CONTROLLER \
      $INCLUDES \
-     $TARGET_COMPILER_OPTIONS \
+     $CONTROLLER_COMPILER_OPTIONS \
      @/root/ultibo/core/fpc/bin/$CFG_NAME \
      $LPR_FILE |& tee -a $LOG && \
 \
-    mv kernel* $LPR_FOLDER/$OUTPUT
+    mv kernel* $ARTIFACTS/$CONTROLLER
     if [[ $? -ne 0 ]]; then log fail: $?; fi
-}
-
-function build-as {
-    local TARGET=$1
-    local FOLDER=$2
-    local REPO=$3
-    if [[ -d $FOLDER ]]
-    then
-        ls $FOLDER/*.lpr > /dev/null 2>&1
-        if [[ $? -eq 0 ]]
-        then
-            rm -rf $FOLDER/$OUTPUT
-            mkdir -p $FOLDER/$OUTPUT
-            local LPR_FILE=$FOLDER/*.lpr
-            case $TARGET in
-                QEMU)
-                    build-lpr $LPR_FILE "-CpARMV7A -WpQEMUVPB" qemuvpb.cfg ;;
-                RPi)
-                    build-lpr $LPR_FILE "-CpARMV6 -WpRPIB" rpi.cfg ;;
-                RPi2)
-                    build-lpr $LPR_FILE "-CpARMV7A -WpRPI2B" rpi2.cfg ;;
-                RPi3)
-                    build-lpr $LPR_FILE "-CpARMV7A -WpRPI3B" rpi3.cfg ;;
-            esac
-            local THISOUT=$OUTPUT/kernels/$FOLDER
-            rm -rf $THISOUT && \
-            mkdir -p $THISOUT && \
-            cp -a $FOLDER/$OUTPUT/* $THISOUT && \
-            if [[ $? -ne 0 ]]; then log fail: $?; fi
-        fi
-    fi
-}
-
-function build-asphyre {
-    local SAMPLES_PATH=Samples/FreePascal/Ultibo
-    for SAMPLE_PATH in $SAMPLES_PATH/*
-    do
-        build-as RPi2 $SAMPLE_PATH ultibohub/Asphyre
-    done
 }
 
 function create-build-summary {
@@ -107,13 +152,14 @@ function create-build-summary {
     fi
 }
 
+ARTIFACTS=artifacts
+OBJ=obj
+SCREEN_NUMBER=1
 ERRORS=build-errors.txt
-OUTPUT=build-output
-LOG=$OUTPUT/build.log
-rm -rf $OUTPUT
-mkdir -p $OUTPUT
-rm -f $LOG
+LOG=$ARTIFACTS/build.log
+rm -rf $ARTIFACTS $OBJ
+mkdir -p $ARTIFACTS $OBJ
 
-build-asphyre
+build-ultibo-asphyre-basic
 
 create-build-summary
